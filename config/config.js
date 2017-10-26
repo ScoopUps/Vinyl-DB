f9f9de60c163f9e4347f9ce0fe459083ac57039a
Jason Soukup <jrsoukup17@gmail.com>
2017-10-26 18:49:42 -0400

    changed db config file

--- config/config.js
+++ config/config.js
@@ -1,24 +1,24 @@
 //set up database configuration export module
 
 //this database is either at a URL or set up locally at a host/port
-module.exports = process.env.DATABASE_URL || {
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || 'vinyl_albums_dev',
-};
+// module.exports = process.env.DATABASE_URL || {
+//   host: process.env.DB_HOST || 'localhost',
+//   port: process.env.DB_PORT || 5432,
+//   database: process.env.DB_NAME || 'vinyl_albums_dev',
+// };
 
-// const pgp = require('pg-promise')();
+const pgp = require('pg-promise')();
 
-// let db;
+let db;
 
-// if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-//   db = pgp({
-//     database: 'vinyl_albums_dev',
-//     port: 5432,
-//     host: 'localhost',
-//   });
-// } else if (process.env.NODE_ENV === 'production') {
-//   db = pgp(process.env.DATABASE_URL);
-// }
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+  db = pgp({
+    database: 'vinyl_albums_dev',
+    port: 5432,
+    host: 'localhost',
+  });
+} else if (process.env.NODE_ENV === 'production') {
+  db = pgp(process.env.DATABASE_URL);
+}
 
-// module.exports = db;
+module.exports = db;
